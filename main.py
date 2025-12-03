7a408af71c6325fee93c12f23b15a2dd2e477020
DrMentalPancke <alexanderadriel09@gmail.com>
2025-12-03 10:49:58 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,71 @@
+from buzzer_music import music
+from machine import Pin, PWM
+import neopixel, time, random
+from time import sleep
+
+# ---------------------
+# Reglur fyrir spilið.
+# ---------------------
+
+#Byrjun:
+#Kastaðu upp á hver byrjar sem fær hæsta byrjar, fyrstur á FINISH reitinn vinnur.
+
+#Sprengju Reitur:
+#Þegar lent er á sprengju þarf að kasta aftur ef þú færð þrjá eða lægra farðu aftur um fjóra reiti af þú færð hærra en þrjá farðu fram um tvo reiti.
+
+#Þrauta reitur:
+#Ef ekki er náð að klára þrautina sem þú lentir á slepptu næstu umferð þinni.
+
+
+
+reed = Pin(2, Pin.IN, Pin.PULL_UP)
+hatalari = PWM(Pin(10), 20000)
+nol = 24
+NeoPin = Pin(14)
+np = neopixel.NeoPixel(NeoPin, nol)
+button = Pin(13, Pin.IN, Pin.PULL_UP)
+
+
+
+
+
+
+# Noturnar fyrir lagið
+
+song = '0 D5 4 14;4 A5 4 14;8 C6 4 14;12 B5 4 14;16 G5 2 14;18 F5 2 14;20 E5 2 14;22 F5 2 14;24 G5 8 14;4 E5 8 16;4 C5 8 16;4 F4 8 16;12 D5 8 16;12 B4 8 16;12 E4 8 16;20 C5 8 16;20 A4 8 16;20 D4 8 16;0 E4 4 16;0 B4 4 16;28 E4 4 16;28 B4 4 16'
+
+
+mySong = music(song, looping=False, pins=[Pin(10)])
+
+
+
+np.fill((0,0,0))
+np.write()
+
+# kóðinn sem lætur lagið spilast
+while True:    
+    if not mySong.tick():   
+        break
+    sleep(0.04)
+
+
+  
+    
+while True:
+    if not reed.value():   # ef reitur er tómur þá er gildið 1 lesið.
+        hatalari.duty(512) 
+        hatalari.freq(440) 
+        print("1")
+    else:
+        hatalari.duty(0)  # slekkur á hátalaranum
+        print("0")        
+        
+
+    if not button.value():          
+        np.fill((0,0,0)) # resettar LEDs
+        led = random.randint(0, nol)
+        np[led] = (0, 100, 0) # kveikir á grænum lit
+        np.write()
+        time.sleep(0.3) # debounce
+    time.sleep(0.01)
+    
